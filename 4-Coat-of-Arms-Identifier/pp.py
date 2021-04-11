4a9882161554bd77a445312c4465a3479131609e
michael-carter-projects <76668887+michael-carter-projects@users.noreply.github.com>
2021-04-10 18:50:16 -0600

    Renamed coa images & write img_src

--- 4-Coat-of-Arms-Identifier/pp.py
+++ 4-Coat-of-Arms-Identifier/pp.py
@@ -1,3 +1,14 @@
 # This script will be used to pre-process the coat of arms images to be used for training.
 #
-#
+#  coa_giovanni_de_medici_pope_leone_x, https://www.papalartifacts.com/the-medici-popes-the-papal-artifacts-collection/
+from os import listdir
+from os.path import isfile, join
+
+img_folder = "coat-of-arms_images"
+filenames = [f for f in listdir(img_folder) if isfile(join(img_folder, f))]
+
+f = open("img_src.txt", "a")
+for fn in filenames:
+    f.write(fn)
+    f.write("\n")
+f.close()
