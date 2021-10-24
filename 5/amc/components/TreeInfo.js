39f13f2cb604d129ec04575414c0adb67691473f
michael-carter-projects <76668887+michael-carter-projects@users.noreply.github.com>
2021-10-24 16:15:22 -0600

    update movement details

--- 5/amc/components/TreeInfo.js
+++ 5/amc/components/TreeInfo.js
@@ -149,9 +149,6 @@ function TreeInfo ({navigation})
 
     for (let i=0; i<alph_movement_names.length; i++) {
       let mvmt_info = movement_details[alph_movement_names[i]];
-
-      console.log("mvmt_info");
-
       all_movements.push(
         <TouchableOpacity key={2*i+1} onPress={() => props.nav.navigate('Movement', {mvmt_info:mvmt_info, prev:'TreeInfo'})}>
           <View  style={{flex:1, flexDirection:'row', marginBottom:sc.margin_width, marginTop:sc.margin_width}}>
